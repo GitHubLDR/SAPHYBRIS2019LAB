5a3452ace34449d44eb60fdb325ff6e711661c25
pradeep <pradeep795387@gmail.com>
2018-10-14 22:03:54 +0530

    springmvchelloweb

---
+++ springmvc/Springmvchelloweb/src/com/tutorials/HelloController.java
@@ -0,0 +1,23 @@
+package com.tutorials;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String printHello(ModelMap model) {
+	      model.addAttribute("textMessage", "Hello open google account");
+	      return "hello";
+	
+	
+	
+	}	
+	
+}
