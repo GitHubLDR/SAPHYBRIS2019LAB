1f1176b379fc89cca62ed237984ff07c89e350c0
user-PC\user <manickyamkarthik@gmail.com>
2018-10-15 12:54:54 +0530

    javaProgram-1

---
+++ JavaPrograms/Javecode/src/com/uhire/main/Main.java
@@ -0,0 +1,18 @@
+package com.uhire.main;
+
+import com.uhire.arthoperations.Arithmetic;
+
+public class Main {
+	
+	 
+	public static void main(String[] args)
+	{
+
+     Arithmetic a1 = new Arithmetic();
+     a1.Add(10, 5);
+     a1.Sub(10, 5);
+     a1.Mul(10, 5);
+     a1.Div(10, 5);
+}
+}
+
