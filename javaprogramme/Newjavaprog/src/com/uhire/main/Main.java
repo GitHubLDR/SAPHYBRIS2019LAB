c356bc2e4cae2add7f4af44723fa2b809405b961
pradeep <pradeep795387@gmail.com>
2018-10-15 16:12:23 +0530

    javanewprog

---
+++ javaprogramme/Newjavaprog/src/com/uhire/main/Main.java
@@ -0,0 +1,18 @@
+package com.uhire.main;
+
+import java.util.Properties;
+
+import com.uhire.operations.Operations;
+
+public class Main {
+public static void main(String[] args) {
+	Operations op1=new Operations();
+	op1.add(5,10);
+	op1.sub(6,34);
+	op1.mul(2,45);
+	op1.div(20,2);
+	
+	
+}
+	
+}
---
+++ javaprogramme/Newjavaprog/src/com/uhire/operations/Operations.java
@@ -0,0 +1,39 @@
+package com.uhire.operations;
+
+public class Operations {
+ 
+	int i,j,q;
+	
+	
+	public void add(int i, int j) {
+		// TODO Auto-generated method stub
+		q=i+j;
+		System.out.println(i+j);
+	
+	}
+
+	public void sub(int i, int j) {
+		// TODO Auto-generated method stub
+		q=i-j;
+		System.out.println(i-j);
+	}
+
+	public void mul(int i, int j) {
+		// TODO Auto-generated method stub
+		q=i*j;
+	System.out.println(i*j);
+	}
+
+	public void div(int i, int j) {
+		// TODO Auto-generated method stub
+	q=i/j;
+		System.out.println(i/j);
+		
+		
+	}
+
+	}
+	
+	
+
+
