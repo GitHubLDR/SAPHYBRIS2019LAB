0f344f7fb0637a6bf9508edae37f3d112ce25458
Surendren <Surenrajagopal007@gmail.com>
2018-10-15 18:50:10 +0530

    ArithmeticOperation

---
+++ JavaProgram/ArithmeticOperation/src/com/uhire/math/operations/ArithmeticMain.java
@@ -0,0 +1,14 @@
+package com.uhire.math.operations;
+
+public class ArithmeticMain {
+
+	public static void main(String[] args) {
+		
+		Operation op1=new Operation();
+		op1.add(5,6);
+		op1.sub(8,4);
+		op1.div(6,3);
+		op1.mul(4,6);
+	}
+
+}
---
+++ JavaProgram/ArithmeticOperation/src/com/uhire/math/operations/Operation.java
@@ -0,0 +1,29 @@
+package com.uhire.math.operations;
+
+public class Operation {
+
+	public void add(int i, int j) {
+		int res = i + j;
+		System.out.println("i + j = " + res);
+		
+	}
+
+	public void sub(int i, int j) {
+		int res = i + j;
+		System.out.println("i - j = " + res);
+		
+	}
+
+	public void div(int i, int j) {
+		int res = i / j;
+		System.out.println("i / j = " + res);
+		
+	}
+
+	public void mul(int i, int j) {
+		int res = i * j;
+		System.out.println("i * j = " +res);
+		
+	}
+
+}
