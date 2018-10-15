f982789a64c63d8aa413febec1a19d51ea0cb0bc
Surendren <Surenrajagopal007@gmail.com>
2018-10-15 18:00:18 +0530

    circulardependency

---
+++ SpringCore/circulardependency/src/com/uhire/util/A.java
@@ -0,0 +1,19 @@
+package com.uhire.util;
+
+import com.uhire.util.B;
+
+public class A {
+	
+	private B b;
+
+	public A(B b) {
+		super();
+		this.b = b;
+	}
+
+	public void show() {
+		System.out.println(b);
+		
+	}
+
+}
---
+++ SpringCore/circulardependency/src/com/uhire/util/B.java
@@ -0,0 +1,20 @@
+package com.uhire.util;
+
+public class B {
+	
+	private A a;
+	
+	
+	public B(A a) {
+		super();
+		this.a = a;
+	}
+
+	public void show() {
+		System.out.println(a);
+		
+	}
+
+
+
+}
