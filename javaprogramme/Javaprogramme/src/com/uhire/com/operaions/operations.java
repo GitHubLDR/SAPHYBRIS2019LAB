d545c4210d6f7593e9e51e0127194c99e5b55d62
sneha <sneha99647@gmail.com>
2018-10-15 15:59:21 +0530

    javaprogramme

---
+++ javaprogramme/Javaprogramme/src/com/uhire/com/operaions/operations.java
@@ -0,0 +1,42 @@
+package com.uhire.com.operaions;
+
+public class operations {
+
+	int i,j,k;
+
+	public void add(int i, int j) {
+		
+		 k=i+j;
+		System.out.println(i+j);
+	}
+
+	public void sub(int i, int j) {
+		// TODO Auto-generated method stub
+		k=i-j;
+		System.out.println(i-j);
+	}
+
+	public void mul(int i, int j) {
+		// TODO Auto-generated method stub
+		k=i*j;
+		System.out.println(i*j);
+	}
+
+	public void div(int i, int j) {
+		// TODO Auto-generated method stub
+		k=i/j;
+		System.out.println(i/j);
+		
+	}
+
+		
+	
+
+	
+	}
+
+	
+	
+	
+	
+
---
+++ javaprogramme/Javaprogramme/src/com/uhire/main/Main.java
@@ -0,0 +1,25 @@
+package com.uhire.main;
+
+import com.uhire.com.operaions.operations;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		operations op= new operations();
+		op.add(10,13);
+		op.sub(20,83);
+		op.mul(98,38);
+		op.div(22,2);
+
+
+		
+		
+		
+		
+		
+	}
+
+}
