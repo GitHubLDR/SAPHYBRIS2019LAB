03fd7fcd957a6ceb66a1edcd502852eb8078cc4c
Adhilakshmi <puttuaudilakshmi@gmail.com>
2018-10-16 23:35:37 +0530

    Calculations

---
+++ Java/Calculations/src/com/main/Main.java
@@ -0,0 +1,19 @@
+package com.main;
+
+import com.math.operations.Operations;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Operations o=new Operations();
+
+		o.add(4,5);
+		o.sub(10,5);
+		o.Mul(2,5);
+		o.Quotient(10,2);
+		o.Reminder(50,5);
+	}
+
+
+}
---
+++ Java/Calculations/src/com/math/operations/Operations.java
@@ -0,0 +1,39 @@
+package com.math.operations;
+
+public class Operations {
+
+	public void add(int i, int j) {
+		
+		int x=i+j;
+		System.out.println("Add :"+x);
+		
+	}
+
+	public void sub(int i, int j) {
+		int x=i-j;
+		System.out.println("Sub :"+x);
+		
+	}
+
+	public void Mul(int i, int j) {
+		
+		int x=i*j;
+		System.out.println("Multiplication :"+x);
+		
+	}
+
+	public void Quotient(int i, int j) {
+		
+		int z=i/j;
+		System.out.println("Quotient :"+z);
+		
+	}
+
+	public void Reminder(int i, int j) {
+		
+		int r=i%j;
+		System.out.println("Reminder :"+r);
+		
+	}
+
+}
