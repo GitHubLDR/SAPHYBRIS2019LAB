f53b20c4ea71f980d8291fa9fd75e22bc7130a74
pradeep&sneha <pradeep795387@gmail.com>
2018-11-01 13:08:33 -0700

    item.xml

---
+++ hybris/uhire2s/src/com/uhire2s/constants/Uhire2sConstants.java
@@ -0,0 +1,28 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2017 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.uhire2s.constants;
+
+/**
+ * Global class for all Uhire2s constants. You can add global constants for your extension into this class.
+ */
+public final class Uhire2sConstants extends GeneratedUhire2sConstants
+{
+	public static final String EXTENSIONNAME = "uhire2s";
+
+	private Uhire2sConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+
+    public static final String PLATFORM_LOGO_CODE = "uhire2sPlatformLogo";
+}
